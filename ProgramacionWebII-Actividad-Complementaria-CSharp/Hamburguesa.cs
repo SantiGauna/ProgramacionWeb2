99e1d6fc4a9ab0aa352a73ab59bc68809f7e5380
SantiGauna <sgaunamartinez@gmail.com>
2024-03-25 17:41:24 -0300

    Agregando carpetas al repositorio

---
+++ ProgramacionWebII-Actividad-Complementaria-CSharp/Hamburguesa.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProgramacionWebII_Actividad_Complementaria_CSharp
+{
+    public class Hamburguesa
+    {
+        public String Nombre { get; set; }
+        public double Precio { get; set; }
+        public Guid IdHamburguesa { get; set; }
+    }
+}
+
